7e714f2f763560e3ecad8b88e2aadc0d2f77fa4c
Dianka Levchenko <dianka_levchenko@inbox.ru>
2020-11-28 00:35:47 +0300

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>Simple HTML</li>
+        <li>Simple HTML</li>
+        <li>Simple HTML</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
