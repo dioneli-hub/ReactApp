3eb4de3466a2cb15a18479697a8beacf2d6f4084
Dianka Levchenko <dianka_levchenko@inbox.ru>
2020-12-29 16:18:23 +0300

    Added dialogs page layout

--- my-app/src/Components/Dialogs/Dialogs.jsx
+++ my-app/src/Components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import c from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={c.dialogs}>
+            <div className={c.dialogs_items}>
+                <div className={c.dialog}>
+                    Diana
+                </div>
+                <div className={c.dialog + ' ' + c.active}>
+                    Vadim
+                </div>
+                <div className={c.dialog}>
+                    Yana
+                </div>
+                <div className={c.dialog}>
+                    Nastya
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>Hi</div>
+                <div className={c.message}>Hi, how are u?</div>
+                <div className={c.message}>GREAT!!!</div>
+            </div>
         </div>
     )
 };
