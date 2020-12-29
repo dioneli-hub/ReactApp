c357c675d7b187804bdd372b35e6944b947bd111
Dianka Levchenko <dianka_levchenko@inbox.ru>
2020-12-29 14:05:38 +0300

    Prevented page reloading, added Navlinks.

--- my-app/src/Components/Navbar/Navbar.jsx
+++ my-app/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import c from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={c.nav}>
             <div className={c.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={c.active}>Profile</NavLink>
             </div>
             <div className={c.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' activeClassName={c.active}>Messages</NavLink>
             </div>
             <div className={c.item}>
-                <a href='/news'>News</a>
+                <NavLink to='/news' activeClassName={c.active}>News</NavLink>
             </div>
             <div className={c.item}>
-                <a href='/music'>Music</a>
+                <NavLink to='/music' activeClassName={c.active}>Music</NavLink>
             </div>
             <div className={c.item}>
-                <a href='/settings'>Settings</a>
+                <NavLink to='/settings' activeClassName={c.active}>Settings</NavLink>
             </div>
         </nav>
     );
