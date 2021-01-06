0de4fbd8013f9612e53c780ccb416b6b0fe2c730
Dianka Levchenko <dianka_levchenko@inbox.ru>
2021-01-06 15:28:52 +0300

    Created own store.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,27 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/store";
 import App from './App';
-import {onAddMessage, onAddPost, updateNewMessageText, updateNewPostText} from "./redux/state";
 
 
-export let rerenderEntireTree = (state) =>{
+export let rerenderEntireTree = () =>{
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} onAddPost={onAddPost} onAddMessage={onAddMessage} updateNewPostText={updateNewPostText} updateNewMessageText={updateNewMessageText} />
+            <App state={store.getState()} onAddPost={store.onAddPost.bind(store)} onAddMessage={store.onAddMessage.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)} />
         </React.StrictMode>,
         document.getElementById('root')
     );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.state);
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a functionstore.
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
